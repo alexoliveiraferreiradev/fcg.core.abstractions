012b8653112e6b82f88ee33b19c640ab2140422f
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-25 11:54:09 -0300

    feat: adiciona modelo de resposta para erros

---
+++ Responses/ErrorResponse.cs
@@ -0,0 +1,8 @@
+namespace Fcg.Core.Abstractions.Responses
+{
+    public class ErrorResponse
+    {
+        public string Message { get; set; }
+        public IEnumerable<string> Errors { get; set; } = [];
+    }
+}
