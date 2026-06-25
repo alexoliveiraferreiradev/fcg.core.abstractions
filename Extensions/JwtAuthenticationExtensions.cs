339e0c96699dac9f5d04f2dfddfb2ec09193a2d9
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-25 14:48:28 -0300

    feat: adiciona configuração de JWT, dependências e atualiza descrição do pacote

---
+++ Extensions/JwtAuthenticationExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Fcg.Core.Abstractions.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Fcg.Core.Abstractions.Extensions
+{
+    public static class JwtAuthenticationExtensions
+    {
+        public static IServiceCollection AddCustomJwtAuthentication(this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            
+            var jwtSettingsSection = configuration.GetSection("JwtSettings");
+            services.Configure<JwtSettings>(jwtSettingsSection);
+            var jwtSettings = jwtSettingsSection.Get<JwtSettings>();
+
+            
+            if (string.IsNullOrEmpty(jwtSettings?.Secret))
+                throw new ArgumentException("JwtSettings:Secret não foi configurado no appsettings.");
+
+            var key = Encoding.ASCII.GetBytes(jwtSettings.Secret);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtSettings.ValidoEm,
+
+                    ValidateAudience = true,
+                    ValidAudience = jwtSettings.Emissor,
+
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero 
+                };
+            });
+            return services;
+        }
+    }
+}
+}
