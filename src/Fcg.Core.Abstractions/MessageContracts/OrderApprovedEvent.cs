f0845da425ce0955610fad14ea55a0dc41f885d7
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-29 22:21:19 -0300

    feat(contracts): adicionar contrato para evento OrderApprovedEvent

---
+++ src/Fcg.Core.Abstractions/MessageContracts/OrderApprovedEvent.cs
@@ -0,0 +1,14 @@
+﻿using Fcg.Core.Abstractions.Interfaces;
+
+namespace Fcg.Core.Abstractions.MessageContracts
+{
+    public record OrderApprovedEvent(
+        Guid OrderId,
+        Guid UserId,
+        string NomeUsuario,
+        DateTime DataCompra) : IEvent
+    {
+        public Guid EventId { get; init; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+    }
+}
