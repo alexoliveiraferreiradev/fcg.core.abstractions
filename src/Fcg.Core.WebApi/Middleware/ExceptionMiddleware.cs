6f9de6e3438fe133d41cc781fe00cc6892c3c42b
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-03 09:23:52 -0300

    feat: adicionar ExceptionMiddleware para tratamento global de excecoes e atualizar versao

---
+++ src/Fcg.Core.WebApi/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Fcg.Core.Abstractions.Common.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace Fcg.Core.WebApi.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            var statusCode = (int)HttpStatusCode.InternalServerError;
+            var message = "Ocorreu um erro interno no servidor. Tente novamente mais tarde.";
+            if (exception is DomainException)
+            {
+                _logger.LogInformation("Regra de Negócio: {Mensagem}", exception.Message);
+                statusCode = (int)HttpStatusCode.BadRequest;
+                message = exception.Message;
+            }
+            else
+            {
+                _logger.LogError(exception, "Erro Crítico: {Mensagem}", exception.Message);
+            }
+
+
+            context.Response.StatusCode = statusCode;
+            await context.Response.WriteAsJsonAsync(new { erro = message });
+        }
+    }
+}
