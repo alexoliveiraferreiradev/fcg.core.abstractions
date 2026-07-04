d2f6c2d1550ba3f3260063d9b5ceff0f817bedc8
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-04 12:21:45 -0300

    feat(Abstractions): adiciona record OrderFinalizedEvent

---
+++ src/Fcg.Core.Abstractions/MessageContracts/OrderFinalizedEvent.cs
@@ -0,0 +1,17 @@
+﻿using Fcg.Core.Abstractions.Enum;
+using Fcg.Core.Abstractions.Interfaces;
+
+namespace Fcg.Core.Abstractions.MessageContracts
+{
+    public record OrderFinalizedEvent(
+    Guid OrderId,
+    Guid UserId,
+    string EmailUsuario,
+    string NomeUsuario,
+    IEnumerable<Guid> JogosIds,
+    PaymentStatus Status) : IEvent
+    {
+        public Guid EventId { get; init; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+    }
+}
