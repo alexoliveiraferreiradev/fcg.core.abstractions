7403e6d84e402cdba095ebf72ce41d721e6df293
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-13 00:22:11 -0300

    refactor(abstractions): implementar IEvent no DeliveryFailedEvent

--- src/Fcg.Core.Abstractions/MessageContracts/DeliveryFailedEvent.cs
+++ src/Fcg.Core.Abstractions/MessageContracts/DeliveryFailedEvent.cs
@@ -1,7 +1,13 @@
-﻿namespace Fcg.Core.Abstractions.MessageContracts
+﻿using Fcg.Core.Abstractions.Interfaces;
+
+namespace Fcg.Core.Abstractions.MessageContracts
 {
     public record DeliveryFailedEvent(
         Guid OrderId, 
         Guid UserId, 
-        string Reason);
+        string Reason) : IEvent
+    {
+        public Guid EventId { get; init; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+    }
 }
