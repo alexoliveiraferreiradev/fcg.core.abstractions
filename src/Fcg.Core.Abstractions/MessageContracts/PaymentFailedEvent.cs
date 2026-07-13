ed94ea0042e92f7508a09530a0c74770dffd030d
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-13 00:22:24 -0300

    refactor(abstractions): implementar IEvent no PaymentFailedEvent

--- src/Fcg.Core.Abstractions/MessageContracts/PaymentFailedEvent.cs
+++ src/Fcg.Core.Abstractions/MessageContracts/PaymentFailedEvent.cs
@@ -1,8 +1,13 @@
-﻿namespace Fcg.Core.Abstractions.MessageContracts
+﻿using Fcg.Core.Abstractions.Interfaces;
+
+namespace Fcg.Core.Abstractions.MessageContracts
 {
     public record PaymentFailedEvent(
      Guid OrderId,
      Guid UserId,
-     string Reason
- );
+     string Reason) : IEvent
+    {
+        public Guid EventId { get; init; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+    };
 }
