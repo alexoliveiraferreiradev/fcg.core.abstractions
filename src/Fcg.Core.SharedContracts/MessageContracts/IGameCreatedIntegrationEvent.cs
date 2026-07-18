c5837675b13e056551f956da1676fe0ec0f7ccbf
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-18 16:50:30 -0300

    refactor(sharedcontracts): alterar propriedade GameId para lista GamesIds no evento IGameCreatedIntegrationEvent

--- src/Fcg.Core.SharedContracts/MessageContracts/IGameCreatedIntegrationEvent.cs
+++ src/Fcg.Core.SharedContracts/MessageContracts/IGameCreatedIntegrationEvent.cs
@@ -5,7 +5,7 @@ namespace Fcg.Core.SharedContracts.MessageContracts
     public interface IGameCreatedIntegrationEvent : IEvent
     {
         Guid UserId { get; }
-        Guid GameId { get; }
+        IEnumerable<Guid> GamesIds { get; }
         string Name { get; }
         decimal Price { get; }
         string Description { get; }
