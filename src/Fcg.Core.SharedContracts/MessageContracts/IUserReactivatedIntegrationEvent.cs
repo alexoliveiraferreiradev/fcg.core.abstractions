e652e40cacec40897c02a82ab4573b77bfdaf946
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-23 17:56:02 -0300

    feat: adicionar contrato IUserReactivatedIntegrationEvent para evento de reativacao de usuario

---
+++ src/Fcg.Core.SharedContracts/MessageContracts/IUserReactivatedIntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿using Fcg.Core.SharedContracts.Interfaces;
+
+namespace Fcg.Core.SharedContracts.MessageContracts
+{
+    public interface IUserReactivatedIntegrationEvent : IEvent
+    {
+        Guid UserId { get; }
+    }
+}
