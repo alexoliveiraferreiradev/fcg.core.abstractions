8ddeaf55241ba8159da97556f0b00da983c1137c
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-23 17:36:30 -0300

    feat: adicionar contrato IUserDeactivatedIntegrationEvent para evento de inativacao de usuario

---
+++ src/Fcg.Core.SharedContracts/MessageContracts/IUserDeactivatedIntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿using Fcg.Core.SharedContracts.Interfaces;
+
+namespace Fcg.Core.SharedContracts.MessageContracts
+{
+    public interface IUserDeactivatedIntegrationEvent : IEvent
+    {
+        Guid UserId { get; }
+    }
+}
