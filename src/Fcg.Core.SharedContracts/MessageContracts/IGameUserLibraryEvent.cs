784d9688770e6e33a315a8a05d033661129a99d6
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-23 11:02:57 -0300

    feat: adicionar contrato IGameUserLibraryEvent para integracao com User Library

---
+++ src/Fcg.Core.SharedContracts/MessageContracts/IGameUserLibraryEvent.cs
@@ -0,0 +1,15 @@
+﻿using Fcg.Core.SharedContracts.Interfaces;
+
+namespace Fcg.Core.SharedContracts.MessageContracts
+{
+    public interface IGameUserLibraryEvent : IEvent
+    {
+        Guid UserId { get; }    
+        Guid GameId { get; }
+        string Name { get; }
+        bool IsAvaiable { get; }
+        string Description { get; }
+        string Genre { get; }
+        DateTime OccurredAt { get; }
+    }
+}
