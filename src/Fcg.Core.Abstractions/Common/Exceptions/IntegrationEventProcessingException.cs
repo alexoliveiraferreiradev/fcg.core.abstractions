7daaed240586bb092427c0d7616b25209d3c4b09
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-31 11:46:25 -0300

    feat(domain): adicionar excecao IntegrationEventProcessingException

---
+++ src/Fcg.Core.Abstractions/Common/Exceptions/IntegrationEventProcessingException.cs
@@ -0,0 +1,11 @@
+﻿namespace Fcg.Core.Abstractions.Common.Exceptions
+{
+    public class IntegrationEventProcessingException : Exception
+    {
+        /// <summary>
+        /// Cria instância de IntegrationEventProcessingException com mensagem personalizada
+        /// </summary>
+        /// <param name="message"></param>
+        public IntegrationEventProcessingException(string message) : base(message) { }
+    }
+}
