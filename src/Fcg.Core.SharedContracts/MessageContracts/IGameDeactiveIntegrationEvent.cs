7f0e75d6ce49486540eb9b383be8774f0de1b40f
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-08-04 18:11:10 -0300

    feat: adicionar contratos IGameDeactiveIntegrationEvent e IGameReactiveIntegrationEvent

---
+++ src/Fcg.Core.SharedContracts/MessageContracts/IGameDeactiveIntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿using Fcg.Core.SharedContracts.Interfaces;
+
+namespace Fcg.Core.SharedContracts.MessageContracts
+{
+    public interface IGameDeactiveIntegrationEvent : IEvent
+    {
+        Guid GameId { get; }
+        DateTime OccurredAt { get; }
+    }
+}
---
+++ src/Fcg.Core.SharedContracts/MessageContracts/IGameReactiveIntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿using Fcg.Core.SharedContracts.Interfaces;
+
+namespace Fcg.Core.SharedContracts.MessageContracts
+{
+    public interface IGameReactiveIntegrationEvent : IEvent
+    {
+        Guid GameId { get; }
+        DateTime OccurredAt { get; }
+    }
+}
