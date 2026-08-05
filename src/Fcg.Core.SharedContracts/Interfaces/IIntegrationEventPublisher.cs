53d002664141240c099f32aea8afcc416f0e3be7
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-08-04 23:20:32 -0300

    feat(application): adiciona interface IIntegrationEventPublisher ao projeto SharedContracts

---
+++ src/Fcg.Core.SharedContracts/Interfaces/IIntegrationEventPublisher.cs
@@ -0,0 +1,7 @@
+﻿namespace Fcg.Core.SharedContracts.Interfaces
+{
+    internal interface IIntegrationEventPublisher
+    {
+        Task PublishAsync<T>(object integrationEvent, CancellationToken cancellationToken = default) where T : class;
+    }
+}
